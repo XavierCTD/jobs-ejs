949e1ee260deaf553753bc3108c2d6643fc22987
XavierCTD <macfamlove01@gmail.com>
2026-02-15 18:22:38 -0500

    Week-12 (WIP).

---
+++ app.js
@@ -0,0 +1,39 @@
+const expess = require("express");
+const app = expess();
+
+app.set("view engine", "ejs");
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+// secret word handling
+let secretWord = "syzygy";
+app.get("/secretWord", (req, res) => {
+  res.render("secretWord", { secretWord });
+});
+
+app.post("/secretWord", (req, res) => {
+  secretWord = req.body.secretWord;
+  res.redirect("/secretWord");
+});
+
+app.use((req, res) => {
+  res.status(404).send(`That page (${req.url}) does not exist!`);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+  console.log(err);
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
