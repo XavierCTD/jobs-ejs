f2e9c92ce3f5c0d9fc61cfa3a41420489a1f60f1
XavierCTD <macfamlove01@gmail.com>
2026-02-22 19:10:38 -0500

    Final Project (WIP)

--- React Folder/dist/assets/index.js
+++ React Folder/dist/assets/index.js
@@ -1,6 +1,6 @@
 // I need to export the React app from the index.jsx file so that it can be imported in the index.html file and rendered to the DOM.
 
-const javascriptx = require("./pages/index.jsx");
+const javascriptx = require("../src/index.jsx");
 
 module.exports = {
   javascriptx,
--- React Folder/src/App.jsx
+++ React Folder/src/App.jsx
@@ -4,8 +4,8 @@ export default function App() {
       <h1>Welcome to CoderPlanet-X!</h1>
       <p>You are authenticated and inside the React app.</p>
       <div className="actions">
-        <a href="/">Back to Express Home</a>
-        <a href="/secretWord">Open Secret Word Page</a>
+        <a href="/">Home</a>
+        <a href="/secretWord">Secret Word</a>
       </div>
       <form method="POST" action="/sessions/logoff">
         <button type="submit">Log out</button>
